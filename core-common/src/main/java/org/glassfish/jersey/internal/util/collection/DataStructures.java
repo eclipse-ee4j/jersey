d3d8fdff5f231fb6b01c1462486e20abd5a20446
mszabo-wikia <mszabo@wikia-inc.com>
2018-10-31 12:58:22 +0100

    Remove LinkedTransferQueue and dependencies
    Signed-off-by: mszabo-wikia <mszabo@wikia-inc.com>

--- core-common/src/main/java/org/glassfish/jersey/internal/util/collection/DataStructures.java
+++ core-common/src/main/java/org/glassfish/jersey/internal/util/collection/DataStructures.java
@@ -16,19 +16,6 @@
 
 package org.glassfish.jersey.internal.util.collection;
 
-import java.security.AccessController;
-import java.security.PrivilegedActionException;
-import java.security.PrivilegedExceptionAction;
-import java.util.Map;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import org.glassfish.jersey.internal.util.JdkVersion;
-
 /**
  * Utility class, which tries to pickup the best collection implementation depending
  * on running environment.
@@ -39,33 +26,6 @@ import org.glassfish.jersey.internal.util.JdkVersion;
  */
 public final class DataStructures {
 
-    private static final Class<?> LTQ_CLASS;
-
-    static {
-        String className = null;
-
-        Class<?> c;
-        try {
-            final JdkVersion jdkVersion = JdkVersion.getJdkVersion();
-            final JdkVersion minimumVersion = JdkVersion.parseVersion("1.7.0");
-
-            className = (minimumVersion.compareTo(jdkVersion) <= 0)
-                    ? "java.util.concurrent.LinkedTransferQueue"
-                    : "org.glassfish.jersey.internal.util.collection.LinkedTransferQueue";
-
-            c = getAndVerify(className);
-            Logger.getLogger(DataStructures.class.getName()).log(Level.FINE, "USING LTQ class:{0}", c);
-        } catch (final Throwable t) {
-            Logger.getLogger(DataStructures.class.getName()).log(Level.FINE,
-                    "failed loading data structure class:" + className
-                            + " fallback to embedded one", t);
-
-            c = LinkedBlockingQueue.class; // fallback to LinkedBlockingQueue
-        }
-
-        LTQ_CLASS = c;
-    }
-
     /**
      * Default concurrency level calculated based on the number of available CPUs.
      */
@@ -74,49 +34,5 @@ public final class DataStructures {
     private static int ceilingNextPowerOfTwo(final int x) {
         // Hacker's Delight, Chapter 3, Harry S. Warren Jr.
         return 1 << (Integer.SIZE - Integer.numberOfLeadingZeros(x - 1));
-    }
-
-    private static Class<?> getAndVerify(final String cn) throws Throwable {
-        try {
-            return AccessController.doPrivileged(new PrivilegedExceptionAction<Class<?>>() {
-                @Override
-                public Class<?> run() throws Exception {
-                    return DataStructures.class.getClassLoader().loadClass(cn).newInstance().getClass();
-                }
-            });
-        } catch (final PrivilegedActionException ex) {
-            throw ex.getCause();
-        }
-    }
-
-    /**
-     * Create an instance of a {@link BlockingQueue} that is based on
-     * {@code LinkedTransferQueue} implementation from JDK 7.
-     * <p>
-     * When running on JDK 7 or higher, JDK {@code LinkedTransferQueue} implementation is used,
-     * on JDK 6 an internal Jersey implementation class is used.
-     * </p>
-     *
-     * @param <E> the type of elements held in the queue.
-     * @return new instance of a {@link BlockingQueue} that is based on {@code LinkedTransferQueue}
-     *         implementation from JDK 7.
-     */
-    @SuppressWarnings("unchecked")
-    public static <E> BlockingQueue<E> createLinkedTransferQueue() {
-        try {
-            return AccessController.doPrivileged(new PrivilegedExceptionAction<BlockingQueue<E>>() {
-                @Override
-                public BlockingQueue<E> run() throws Exception {
-                    return (BlockingQueue<E>) LTQ_CLASS.newInstance();
-                }
-            });
-        } catch (final PrivilegedActionException ex) {
-            final Throwable cause = ex.getCause();
-            if (cause instanceof RuntimeException) {
-                throw (RuntimeException) cause;
-            } else {
-                throw new RuntimeException(cause);
-            }
-        }
     }
 }
