a0e276e9219677d601939b5623412543f1bea2f6
pappy <pappy.stanescu@gmail.com>
2019-08-28 13:12:02 +0200

    #4208: Implemented getBeanClass in SupplierBeanBridge.
    The method JersetBean.getBeanClass is used to compute the bean
    identifier. The default id for a JerseyBean<T> is
    "java.lang.Object#jersey", causing collisions of bridge instances.
    
    In particular, for HelloWorldResourceTest, Weld's MapBeanStore returned
    a SupplierInstanceBeanBridge instead of the expected SupplierBeanBridge,
    because both shared the same bean identifier.
    
    Signed-off-by: pappy <pappy.stanescu@gmail.com>

--- inject/cdi2-se/src/main/java/org/glassfish/jersey/inject/cdi/se/bean/SupplierBeanBridge.java
+++ inject/cdi2-se/src/main/java/org/glassfish/jersey/inject/cdi/se/bean/SupplierBeanBridge.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -142,5 +142,12 @@ class SupplierBeanBridge extends JerseyBean<Object> {
     @SuppressWarnings("unchecked")
     public Class<? extends Annotation> getScope() {
         return binding.getScope() == null ? Dependent.class : transformScope(binding.getScope());
+    }
+
+    @Override
+    public Class<?> getBeanClass() {
+        return this.binding.getContracts().isEmpty()
+                ? super.getBeanClass()
+                : (Class<?>) this.binding.getContracts().iterator().next();
     }
 }
