918225d8eb5c587aee5c5c553e11ca996167ace6
Marcelo Rubim <marcelorubim@gmail.com>
2019-09-11 21:09:54 -0300

    Fixed the copyright owner
    Signed-off-by: Marcelo Rubim <marcelorubim@gmail.com>

--- connectors/jetty-connector/src/test/java/org/glassfish/jersey/jetty/connector/ProxyTest.java
+++ connectors/jetty-connector/src/test/java/org/glassfish/jersey/jetty/connector/ProxyTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019 Banco do Brasil S/A. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -12,6 +12,8 @@
  * https://www.gnu.org/software/classpath/license.html.
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ * Contributors:
+ *    Marcelo Rubim
  */
 package org.glassfish.jersey.jetty.connector;
 
