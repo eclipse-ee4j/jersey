e0fa5ca6baa779367e11bfc1fb2e0affff03a87d
Maxim Nesen <maxim.nesen@oracle.com>
2019-09-30 20:26:44 +0200

    Test with MultiValueHashMap
    Signed-off-by: Maxim Nesen <maxim.nesen@oracle.com>

--- media/json-binding/src/test/java/org/glassfish/jersey/jsonb/internal/JsonBindingProviderTest.java
+++ media/json-binding/src/test/java/org/glassfish/jersey/jsonb/internal/JsonBindingProviderTest.java
@@ -29,6 +29,7 @@ import java.util.Map;
 import java.util.Set;
 
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.NoContentException;
 import javax.ws.rs.ext.ContextResolver;
@@ -54,7 +55,7 @@ public final class JsonBindingProviderTest {
 
         // when
         mbr.readFrom(Foo.class, Foo.class, new Annotation[0], APPLICATION_JSON_TYPE,
-                new EmptyMultivaluedMap<String, String>(), new ByteArrayInputStream(new byte[0]));
+                new MultivaluedHashMap<>(), new ByteArrayInputStream(new byte[0]));
 
         // then
         // should throw NoContentException
@@ -86,98 +87,6 @@ public final class JsonBindingProviderTest {
         @Override
         public final <T> ContextResolver<T> getContextResolver(final Class<T> contextType, final MediaType mediaType) {
             return null;
-        }
-
-    }
-
-    private static final class EmptyMultivaluedMap<K, V> implements MultivaluedMap<K, V> {
-
-        @Override
-        public final int size() {
-            return 0;
-        }
-
-        @Override
-        public final boolean isEmpty() {
-            return true;
-        }
-
-        @Override
-        public final boolean containsKey(final Object key) {
-            return false;
-        }
-
-        @Override
-        public final boolean containsValue(final Object value) {
-            return false;
-        }
-
-        @Override
-        public final List<V> get(final Object key) {
-            return null;
-        }
-
-        @Override
-        public final List<V> put(final K key, final List<V> value) {
-            return null;
-        }
-
-        @Override
-        public final List<V> remove(final Object key) {
-            return null;
-        }
-
-        @Override
-        public final void putAll(final Map<? extends K, ? extends List<V>> m) {
-        }
-
-        @Override
-        public final void clear() {
-        }
-
-        @Override
-        public final Set<K> keySet() {
-            return Collections.emptySet();
-        }
-
-        @Override
-        public final Collection<List<V>> values() {
-            return Collections.emptySet();
-        }
-
-        @Override
-        public final Set<Entry<K, List<V>>> entrySet() {
-            return Collections.emptySet();
-        }
-
-        @Override
-        public final void putSingle(final K key, final V value) {
-        }
-
-        @Override
-        public final void add(final K key, final V value) {
-        }
-
-        @Override
-        public final V getFirst(final K key) {
-            return null;
-        }
-
-        @Override
-        public final void addAll(final K key, final V... newValues) {
-        }
-
-        @Override
-        public final void addAll(final K key, final List<V> valueList) {
-        }
-
-        @Override
-        public final void addFirst(final K key, final V value) {
-        }
-
-        @Override
-        public final boolean equalsIgnoreValueOrder(final MultivaluedMap<K, V> otherMap) {
-            return false;
         }
 
     }
