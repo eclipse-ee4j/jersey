b43b8236889edd6767a2a5a2c62d8bb39b5ea743
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2019-11-27 15:19:10 +0000

    Fixing check styles (3)
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- tests/integration/property-check/src/test/java/org/glassfish/jersey/internal/config/SystemPropertiesConfigurationModelTest.java
+++ tests/integration/property-check/src/test/java/org/glassfish/jersey/internal/config/SystemPropertiesConfigurationModelTest.java
@@ -20,30 +20,32 @@ public class SystemPropertiesConfigurationModelTest {
 
     @Test
     public void allPropertyClassLoaded() throws IOException {
-        Predicate<Class<?>> containsAnnotation = clazz -> clazz.getAnnotation(PropertiesClass.class) != null || clazz.getAnnotation(Property.class) != null;
+        Predicate<Class<?>> containsAnnotation = clazz -> clazz.getAnnotation(PropertiesClass.class) != null
+                || clazz.getAnnotation(Property.class) != null;
         Predicate<Class<?>> notCommon = clazz -> clazz != CommonProperties.class;
         Predicate<Class<?>> notTestProperties = clazz -> clazz != TestProperties.class;
-        List<String> propertyClasses = getClassesWithPredicate("org.glassfish.jersey", notCommon, notTestProperties, containsAnnotation).stream()
-                .map(Class::getName).collect(Collectors.toList());
+        List<String> propertyClasses = getClassesWithPredicate("org.glassfish.jersey", notCommon, notTestProperties,
+                containsAnnotation).stream().map(Class::getName).collect(Collectors.toList());
         propertyClasses.removeAll(SystemPropertiesConfigurationModel.PROPERTY_CLASSES);
         assertEquals("New properties have been found. "
-                + "Make sure you add next classes in SystemPropertiesConfigurationModel.PROPERTY_CLASSES: "+propertyClasses, 0, propertyClasses.size());
+                + "Make sure you add next classes in SystemPropertiesConfigurationModel.PROPERTY_CLASSES: "
+                + propertyClasses, 0, propertyClasses.size());
     }
-    
-    private List<Class<?>> getClassesWithPredicate(String packageRoot, Predicate<Class<?>> ... predicates) throws IOException{
+
+    private List<Class<?>> getClassesWithPredicate(String packageRoot, Predicate<Class<?>>... predicates)
+            throws IOException {
         ClassPath classpath = ClassPath.from(Thread.currentThread().getContextClassLoader());
-        Stream<Class<?>> steam = classpath.getTopLevelClassesRecursive(packageRoot)
-                .stream().map(classInfo -> {
-                    try {
-                        return Class.forName(classInfo.getName());
-                    } catch (ClassNotFoundException e) {
-                        return null;
-                    }
-                });
+        Stream<Class<?>> steam = classpath.getTopLevelClassesRecursive(packageRoot).stream().map(classInfo -> {
+            try {
+                return Class.forName(classInfo.getName());
+            } catch (ClassNotFoundException e) {
+                return null;
+            }
+        });
         for (Predicate<Class<?>> predicate : predicates) {
             steam = steam.filter(predicate);
         }
         return steam.collect(Collectors.toList());
     }
-    
+
 }
