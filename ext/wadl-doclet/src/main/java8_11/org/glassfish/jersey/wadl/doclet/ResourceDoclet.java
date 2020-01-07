be5f31a59cdf61a8ab38d1cef18ef263f747ae7c
jbescos <jorge.bescos.gascon@oracle.com>
2020-01-07 19:59:49 +0100

    Fix check style (#4359)
    * Fix check style
    
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- ext/wadl-doclet/src/main/java8_11/org/glassfish/jersey/wadl/doclet/ResourceDoclet.java
+++ ext/wadl-doclet/src/main/java8_11/org/glassfish/jersey/wadl/doclet/ResourceDoclet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -388,7 +388,6 @@ public class ResourceDoclet {
 
         /* Get referenced example bean
          */
-        
         final ClassDoc containingClass = referencedMember.containingClass();
         return DocletUtils.getLinkClass(containingClass.qualifiedName(), referencedMember.name());
     }
