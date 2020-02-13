d06d8e3d694025b0452b9ee796ed98534ac63ceb
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2020-02-13 12:29:42 +0100

    Update copyright and more readable string format
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- connectors/netty-connector/src/main/java/org/glassfish/jersey/netty/connector/NettyConnector.java
+++ connectors/netty-connector/src/main/java/org/glassfish/jersey/netty/connector/NettyConnector.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -285,7 +285,7 @@ class NettyConnector implements Connector {
 
     private String buildPathWithQueryParameters(URI requestUri) {
         if (requestUri.getRawQuery() != null) {
-            return new StringBuilder(requestUri.getRawPath()).append("?").append(requestUri.getRawQuery()).toString();
+            return String.format("%s?%s", requestUri.getRawPath(), requestUri.getRawQuery());
         } else {
             return requestUri.getRawPath();
         }
