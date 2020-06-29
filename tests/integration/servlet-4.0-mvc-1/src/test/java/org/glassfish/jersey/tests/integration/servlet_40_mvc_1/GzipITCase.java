c8df9a962940b837927a1b6bc79cf9e4eddcd76c
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2020-06-29 10:56:58 +0200

    Correct name of packages
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- tests/integration/servlet-4.0-mvc-1/src/test/java/org/glassfish/jersey/tests/integration/servlet_40_mvc_1/GzipITCase.java
+++ tests/integration/servlet-4.0-mvc-1/src/test/java/org/glassfish/jersey/tests/integration/servlet_40_mvc_1/GzipITCase.java
@@ -14,7 +14,7 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
-package org.glassfish.jersey.tests.integration.servlet_25_mvc_4;
+package org.glassfish.jersey.tests.integration.servlet_40_mvc_1;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
