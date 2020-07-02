ba14d86bcb3b895822c747b4fd06668bd9e4aa56
Miguel Serra <Miguel.Serra@criticaltechworks.com>
2020-07-02 16:31:33 +0100

    Regexp on MP RestClient @Path
    Signed-off-by: Miguel Serra <Miguel.Serra@criticaltechworks.com>

--- tests/integration/microprofile/rest-client/src/test/java/org/glassfish/jersey/restclient/ApplicationResourceImpl.java
+++ tests/integration/microprofile/rest-client/src/test/java/org/glassfish/jersey/restclient/ApplicationResourceImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -62,4 +62,13 @@ public class ApplicationResourceImpl implements ApplicationResource {
         return null;
     }
 
+    @Override
+    public String regex(String content) {
+        return content;
+    }
+
+    @Override
+    public String regex0(String context0, String context1) {
+        return context0 + "_" + context1;
+    }
 }
