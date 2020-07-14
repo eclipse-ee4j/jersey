907718f378619737f48f942550cad7a3d0293902
Thibault Vallin <thibault.vallin@oracle.com>
2020-07-14 13:04:26 +0200

    microprofile REST client Example - initial commit
    Signed-off-by: tvallin <thibault.vallin@oracle.com>

---
+++ examples/bookshop-microprofile/src/main/java/org/glassfish/jersey/example/bookshop/microprofile/server/BookingFeatures.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0, which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.glassfish.jersey.example.bookshop.microprofile.server;
+
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.MatrixParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URISyntaxException;
+import java.util.Date;
+
+/**
+ * Interface which gather the booking method used by microprofile rest client
+ */
+public interface BookingFeatures {
+
+    @POST
+    @Path("/{customerName}/{bookName}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    Response reserveBookByName(@PathParam("customerName") String customerName, @PathParam("bookName") String bookName,
+                               @BeanParam Date fromDate, Date toDate) throws URISyntaxException;
+
+}
---
+++ examples/bookshop-microprofile/src/main/java/org/glassfish/jersey/example/bookshop/microprofile/server/LibraryFeatures.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0, which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package org.glassfish.jersey.example.bookshop.microprofile.server;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Interface which gather the Library method used by microprofile rest client
+ */
+public interface LibraryFeatures {
+
+    @POST
+    @Path("/isCustomer")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    boolean isRegistered(String customer);
+
+    @POST
+    @Path("/containsBook")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    boolean containsBook(String bookName);
+
+    @POST
+    @Path("/register/{customerName}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    Response registerCustomer(@PathParam("customerName") String customerName);
+
+}
