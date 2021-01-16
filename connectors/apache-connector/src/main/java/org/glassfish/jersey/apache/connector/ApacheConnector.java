c10d88460c782385ab031ac88388e3c1c39b1ce4
Nicholas DiPiazza <nicholas.dipiazza@gmail.com>
2021-01-15 21:27:16 -0600

    Add two missing configuration for Apache - DefaultAuthSchemeRegistry and RedirectStrategy
    Signed-off-by: Nicholas DiPiazza <nicholas.dipiazza@lucidworks.com>

--- connectors/apache-connector/src/main/java/org/glassfish/jersey/apache/connector/ApacheConnector.java
+++ connectors/apache-connector/src/main/java/org/glassfish/jersey/apache/connector/ApacheConnector.java
@@ -62,6 +62,7 @@ import org.apache.http.ConnectionReuseStrategy;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthSchemeProvider;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.AuthCache;
@@ -69,6 +70,7 @@ import org.apache.http.client.CookieStore;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.HttpRequestRetryHandler;
+import org.apache.http.client.RedirectStrategy;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -249,6 +251,34 @@ class ApacheConnector implements Connector {
             }
         }
 
+        Object redirectStrategy = config.getProperties().get(ApacheClientProperties.REDIRECT_STRATEGY);
+        if (redirectStrategy != null) {
+            if (!(redirectStrategy instanceof RedirectStrategy)) {
+                LOGGER.log(
+                    Level.WARNING,
+                    LocalizationMessages.IGNORING_VALUE_OF_PROPERTY(
+                        ApacheClientProperties.REDIRECT_STRATEGY,
+                        redirectStrategy.getClass().getName(),
+                        RedirectStrategy.class.getName())
+                );
+                redirectStrategy = null;
+            }
+        }
+
+        Object defaultAuthSchemeRegistry = config.getProperties().get(ApacheClientProperties.DEFAULT_AUTH_SCHEME_REGISTRY);
+        if (defaultAuthSchemeRegistry != null) {
+            if (!(defaultAuthSchemeRegistry instanceof Registry)) {
+                LOGGER.log(
+                    Level.WARNING,
+                    LocalizationMessages.IGNORING_VALUE_OF_PROPERTY(
+                        ApacheClientProperties.DEFAULT_AUTH_SCHEME_REGISTRY,
+                        defaultAuthSchemeRegistry.getClass().getName(),
+                        Registry.class.getName())
+                );
+                defaultAuthSchemeRegistry = null;
+            }
+        }
+
         final SSLContext sslContext = client.getSslContext();
         final HttpClientBuilder clientBuilder = HttpClientBuilder.create();
 
@@ -315,6 +345,14 @@ class ApacheConnector implements Connector {
                 requestConfigBuilder.setCookieSpec(CookieSpecs.IGNORE_COOKIES);
             }
             requestConfig = requestConfigBuilder.build();
+        }
+
+        if (redirectStrategy != null) {
+            clientBuilder.setRedirectStrategy((RedirectStrategy) redirectStrategy);
+        }
+
+        if (defaultAuthSchemeRegistry != null) {
+            clientBuilder.setDefaultAuthSchemeRegistry((Registry<AuthSchemeProvider>) defaultAuthSchemeRegistry);
         }
 
         if (requestConfig.getCookieSpec() == null || !requestConfig.getCookieSpec().equals(CookieSpecs.IGNORE_COOKIES)) {
