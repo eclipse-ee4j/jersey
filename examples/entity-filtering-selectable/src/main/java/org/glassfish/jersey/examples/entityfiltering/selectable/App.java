741aad4fb064c1824325339f78d22b2a5c3dac20
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2021-01-26 14:34:03 +0100

    Apply 2021
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- examples/entity-filtering-selectable/src/main/java/org/glassfish/jersey/examples/entityfiltering/selectable/App.java
+++ examples/entity-filtering-selectable/src/main/java/org/glassfish/jersey/examples/entityfiltering/selectable/App.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
