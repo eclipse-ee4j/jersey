9e179cec3d72fc38c14615b6a3f2c1dcf8b43b91
Maxim Nesen <maxim.nesen@oracle.com>
2021-02-11 16:21:32 +0100

    Platform independent space
    Signed-off-by: Maxim Nesen <maxim.nesen@oracle.com>

--- archetypes/jersey-quickstart-grizzly2/src/main/resources/archetype-resources/src/main/java/Main.java
+++ archetypes/jersey-quickstart-grizzly2/src/main/resources/archetype-resources/src/main/java/Main.java
@@ -37,7 +37,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with endpoints available at "
-                + "%s\nHit Ctrl-C to stop it...", BASE_URI));
+                + "%s%nHit Ctrl-C to stop it...", BASE_URI));
         System.in.read();
         server.stop();
     }
