f0e0106dc24663294fdc9d787981c118c2236d99
Maxim Nesen <maxim.nesen@oracle.com>
2021-02-26 13:50:07 +0100

    processing order for Jackson/Jaxb annotations
    Signed-off-by: Maxim Nesen <maxim.nesen@oracle.com>

--- media/json-jackson/src/test/java/org/glassfish/jersey/jackson/internal/model/ServiceTest.java
+++ media/json-jackson/src/test/java/org/glassfish/jersey/jackson/internal/model/ServiceTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -23,6 +23,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.xml.bind.annotation.XmlElement;
 
 @Path("/entity/")
 public final class ServiceTest {
@@ -45,6 +46,7 @@ public final class ServiceTest {
             this.value = value;
         }
 
+        @XmlElement(name = "jaxb")
         @JsonGetter("name")
         public final String getName() {
             return name;
