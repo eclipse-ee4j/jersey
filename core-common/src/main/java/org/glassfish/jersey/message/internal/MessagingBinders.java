7b6d2f84391f24310baae7d8e55e644703642826
jansupol <jan.supol@oracle.com>
2021-04-09 15:37:38 +0200

    Add a default Enum MB provider.
    Signed-off-by: jansupol <jan.supol@oracle.com>

--- core-common/src/main/java/org/glassfish/jersey/message/internal/MessagingBinders.java
+++ core-common/src/main/java/org/glassfish/jersey/message/internal/MessagingBinders.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -89,6 +89,7 @@ public final class MessagingBinders {
             bindSingletonWorker(ReaderProvider.class);
             // bindSingletonWorker(RenderedImageProvider.class); - enabledProvidersBinder
             bindSingletonWorker(StringMessageProvider.class);
+            bindSingletonWorker(EnumMessageProvider.class);
 
             // Message body readers -- enabledProvidersBinder
             // bind(SourceProvider.StreamSourceReader.class).to(MessageBodyReader.class).in(Singleton.class);
