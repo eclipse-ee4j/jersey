fd9045041a45f986b9a6978f4b30264b07b1a9c0
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2021-07-02 11:07:28 +0200

    Include error stacktrace in jenkins
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- etc/jenkins/jenkins_build.sh
+++ etc/jenkins/jenkins_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
 
-mvn -V -U -B clean install glassfish-copyright:check -Dcopyright.quiet=false -Dsurefire.systemPropertiesFile=${WORKSPACE}/etc/jenkins/systemPropertiesFile
+mvn -e -V -U -B clean install glassfish-copyright:check -Dcopyright.quiet=false -Dsurefire.systemPropertiesFile=${WORKSPACE}/etc/jenkins/systemPropertiesFile
