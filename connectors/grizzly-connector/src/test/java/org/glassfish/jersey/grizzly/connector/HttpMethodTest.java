855f0254db34ca8416d961fb8c89937ccac688a7
aserkes <andrii.serkes@oracle.com>
2021-08-12 19:49:22 +0200

    add possibility to use entity with http method Options in requests according to the RFC 7231
    Signed-off-by: aserkes <andrii.serkes@oracle.com>

--- connectors/grizzly-connector/src/test/java/org/glassfish/jersey/grizzly/connector/HttpMethodTest.java
+++ connectors/grizzly-connector/src/test/java/org/glassfish/jersey/grizzly/connector/HttpMethodTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -62,15 +62,6 @@ public class HttpMethodTest extends JerseyTest {
         public String get() {
             return "GET";
         }
-    }
-
-
-    @Test
-    public void testOptions() {
-        WebTarget r = getWebTarget();
-        Response cr = r.request().options();
-        assertTrue(cr.hasEntity());
-        cr.close();
     }
 
     @Test
