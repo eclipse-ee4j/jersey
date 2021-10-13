5d9bfe504ff79f973d571177c5642effa4473bf9
jansupol <jan.supol@oracle.com>
2021-10-13 13:14:53 +0200

    Split MP REST Client integration test module to support javax.servlet in the TCK
    Signed-off-by: jansupol <jan.supol@oracle.com>

--- ext/microprofile/mp-rest-client/src/test/java/org/glassfish/jersey/microprofile/restclient/InterfaceUtilTest.java
+++ ext/microprofile/mp-rest-client/src/test/java/org/glassfish/jersey/microprofile/restclient/InterfaceUtilTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -16,11 +16,11 @@
 
 package org.glassfish.jersey.microprofile.restclient;
 
+import org.junit.Test;
+
 import java.util.Arrays;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class InterfaceUtilTest {
 
