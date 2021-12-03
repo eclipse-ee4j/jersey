b9653d17f98ca41b72172af7028ba046f300f834
robersheimer <richard.obersheimer@anderscore.com>
2021-12-03 19:13:18 +0100

    Issue #3493 - Add BeanParam support to WebResourceFactory (#4919)
    * Issue #3493 - Add BeanParam support to WebResourceFactory
    
    Allows the proxy client holding a resource interface to supply
    a bean of the corresponding type for the method fields annotated
    with @BeanParam, instead of having to supply the associated
    parameters individually. This also works if BeanParams themselves
    contain fields annotated with @BeanParam.

---
+++ ext/proxy-client/src/test/java/org/glassfish/jersey/client/proxy/MyResourceWithBeanParam.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package org.glassfish.jersey.client.proxy;
+
+import jakarta.ws.rs.BeanParam;
+
+/**
+ * @author Richard Obersheimer
+ */
+public class MyResourceWithBeanParam implements MyResourceWithBeanParamIfc {
+
+    @Override
+    public String echoQuery(MyGetBeanParam bean) {
+        return bean.getQueryParam();
+    }
+
+    @Override
+    public String echoHeader(@BeanParam MyGetBeanParam bean) {
+        return bean.getHeaderParam();
+    }
+
+    @Override
+    public String echoPath(@BeanParam MyGetBeanParam bean) {
+        return bean.getPathParam();
+    }
+
+    @Override
+    public String echoCookie(@BeanParam MyGetBeanParam bean) {
+        return bean.getCookieParam().getValue();
+    }
+
+    @Override
+    public String echoMatrix(@BeanParam MyGetBeanParam bean) {
+        return bean.getMatrixParam().toString();
+    }
+
+    @Override
+    public String echoSubBean(@BeanParam MyGetBeanParam bean) {
+        return bean.getSubBeanParam().getSubQueryParam().toString();
+    }
+
+    @Override
+    public String echo(MyBeanParam bean) {
+        return ("HEADER=" + bean.getHeaderParam() + ",PATH=" + bean.getPathParam() + ",FORM="
+                + bean.getFormParam1() + "," + bean.getFormParam2() + ",QUERY=" + bean.getQueryParam()
+                + ",MATRIX=" + bean.getMatrixParam().size() + ",COOKIE=" + bean.getCookieParam().getValue()
+                + ",SUB=" + bean.getSubBeanParam().getSubQueryParam().size()
+                + ",Q2=" + bean.getQueryParam2());
+    }
+
+    @Override
+    public MyResourceWithBeanParamIfc getSubResource() {
+        return new MyResourceWithBeanParam();
+    }
+}
---
+++ ext/proxy-client/src/test/java/org/glassfish/jersey/client/proxy/MyResourceWithBeanParamIfc.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package org.glassfish.jersey.client.proxy;
+
+import jakarta.ws.rs.BeanParam;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+/**
+ * @author Richard Obersheimer
+ */
+@Path("mybeanresource")
+public interface MyResourceWithBeanParamIfc {
+
+    @GET
+    @Path("getQuery")
+    @Produces("text/plain")
+    public String echoQuery(@BeanParam MyGetBeanParam bean);
+
+    @GET
+    @Path("getHeader")
+    @Produces("text/plain")
+    public String echoHeader(@BeanParam MyGetBeanParam bean);
+
+    @GET
+    @Path("getPath/{pathParam}")
+    @Produces("text/plain")
+    public String echoPath(@BeanParam MyGetBeanParam bean);
+
+    @GET
+    @Path("getCookie")
+    @Produces("text/plain")
+    public String echoCookie(@BeanParam MyGetBeanParam bean);
+
+    @GET
+    @Path("getMatrix")
+    @Produces("text/plain")
+    public String echoMatrix(@BeanParam MyGetBeanParam bean);
+
+    @GET
+    @Path("getSubBean")
+    @Produces("text/plain")
+    public String echoSubBean(@BeanParam MyGetBeanParam bean);
+
+    @POST
+    @Consumes("application/x-www-form-urlencoded")
+    @Path("all/{pathParam}")
+    @Produces("text/plain")
+    public String echo(@BeanParam MyBeanParam bean);
+
+    @Path("subresource")
+    MyResourceWithBeanParamIfc getSubResource();
+
+}
