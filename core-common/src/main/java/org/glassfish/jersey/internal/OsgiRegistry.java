74ec97a7b913e80742b41697f8dc1fcc33a1ed44
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2022-03-04 15:21:37 +0100

    ServiceFinder defaults to ServiceLookupIteratorProvider
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- core-common/src/main/java/org/glassfish/jersey/internal/OsgiRegistry.java
+++ core-common/src/main/java/org/glassfish/jersey/internal/OsgiRegistry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2022 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -106,7 +106,7 @@ public final class OsgiRegistry implements SynchronousBundleListener {
 
     private final class OsgiServiceFinder extends ServiceFinder.ServiceIteratorProvider {
 
-        final ServiceFinder.ServiceIteratorProvider defaultIterator = new ServiceFinder.DefaultServiceIteratorProvider();
+        final ServiceFinder.ServiceIteratorProvider defaultIterator = new ServiceFinder.ServiceReflectionIteratorProvider();
 
         @Override
         public <T> Iterator<T> createIterator(
