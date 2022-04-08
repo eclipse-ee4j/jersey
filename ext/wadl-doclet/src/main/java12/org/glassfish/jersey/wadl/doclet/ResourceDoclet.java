8f7fa9917626095a816dc2114a5c1e99a89ea11b
johnmarquez12 <42983443+johnmarquez12@users.noreply.github.com>
2022-04-08 00:03:30 -0700

    Fixed NullpointerException when getting methodtree (#5024)
    Co-authored-by: jm041977 <john.marquez@broadcom.com>

--- ext/wadl-doclet/src/main/java12/org/glassfish/jersey/wadl/doclet/ResourceDoclet.java
+++ ext/wadl-doclet/src/main/java12/org/glassfish/jersey/wadl/doclet/ResourceDoclet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2022 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -162,8 +162,10 @@ public class ResourceDoclet implements Doclet {
                         Map<DocTree.Kind, Map<String, String>> tags = getTags(docTrees.getDocCommentTree(method));
                         MethodTree methodTree = docTrees.getTree(method);
                         MethodDocType methodDocType = new MethodDocType();
-                        methodDocType.setMethodName(methodTree.getName().toString());
-                        methodDocType.setCommentText(getComments(docTrees.getDocCommentTree(method)));
+                        if (methodTree != null) {
+                            methodDocType.setMethodName(methodTree.getName().toString());
+                            methodDocType.setCommentText(getComments(docTrees.getDocCommentTree(method)));
+                        }
                         getTags(docTrees.getDocCommentTree(method));
                         StringBuilder arguments = new StringBuilder("(");
                         Map<String, String> paramTags = tags.get(DocTree.Kind.PARAM);
