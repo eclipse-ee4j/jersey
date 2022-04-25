01b88feb1d160e5f9877c323b3917f7d0878c1d8
jansupol <15908245+jansupol@users.noreply.github.com>
2022-04-25 10:50:35 +0200

    Release MessageBodyWorkers when Response gets closed. (#5039)
    Signed-off-by: jansupol <jan.supol@oracle.com>

--- core-server/src/main/java/org/glassfish/jersey/server/ContainerResponse.java
+++ core-server/src/main/java/org/glassfish/jersey/server/ContainerResponse.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2022 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -388,6 +388,7 @@ public class ContainerResponse implements ContainerResponseContext {
             closed = true;
             messageContext.close();
             requestContext.getResponseWriter().commit();
+            requestContext.setWorkers(null);
         }
     }
 
