b4227a2e9353233fc301c095acd34780ce880e1a
Maxim Nesen <maxim.nesen@oracle.com>
2022-06-07 10:02:53 +0200

    Update Jackson to 2.13.3
    Signed-off-by: Maxim Nesen <maxim.nesen@oracle.com>

--- media/json-jackson/src/main/java/org/glassfish/jersey/jackson/internal/jackson/jaxrs/base/ProviderBase.java
+++ media/json-jackson/src/main/java/org/glassfish/jersey/jackson/internal/jackson/jaxrs/base/ProviderBase.java
@@ -12,7 +12,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
