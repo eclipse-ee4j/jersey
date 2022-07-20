645c15a8537bca580da7f6e6228116f1a26deca3
Gaurav Gupta <gaurav.gupta@payara.fish>
2022-07-20 17:04:25 +0530

    FISH-5723 glassfish-copyright-maven-plugin cp year update

--- core-common/src/main/java/org/glassfish/jersey/internal/util/collection/Cache.java
+++ core-common/src/main/java/org/glassfish/jersey/internal/util/collection/Cache.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2022 Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2022 Payara Foundation and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
