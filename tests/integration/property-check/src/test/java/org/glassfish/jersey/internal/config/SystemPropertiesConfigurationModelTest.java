5bfa401de6cca80c96223f0b70857af53e496224
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2022-08-10 13:35:19 +0200

    Support JDK19
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- tests/integration/property-check/src/test/java/org/glassfish/jersey/internal/config/SystemPropertiesConfigurationModelTest.java
+++ tests/integration/property-check/src/test/java/org/glassfish/jersey/internal/config/SystemPropertiesConfigurationModelTest.java
@@ -44,6 +44,7 @@ import org.glassfish.jersey.server.ServerProperties;
 import org.glassfish.jersey.server.oauth1.OAuth1ServerProperties;
 import org.glassfish.jersey.servlet.ServletProperties;
 import org.glassfish.jersey.test.TestProperties;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SystemPropertiesConfigurationModelTest {
@@ -77,6 +78,7 @@ public class SystemPropertiesConfigurationModelTest {
     }
 
     @Test
+    @Ignore("System.setSecurityManager is deprecated in JDK 19 and there is no replacement")
     public void propertyLoadedWhenSecurityException() {
         final String TEST_STRING = "test";
         SecurityManager sm = System.getSecurityManager();
