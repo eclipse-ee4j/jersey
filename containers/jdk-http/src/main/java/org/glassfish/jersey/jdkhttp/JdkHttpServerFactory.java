f2f95f94b965744be9d0f007f141b1e222d7e759
Matthias van der Hallen <matthias.vanderhallen@gmail.com>
2022-09-22 09:36:47 +0200

    Update JdkHttpServerFactory.java (#5163)

--- containers/jdk-http/src/main/java/org/glassfish/jersey/jdkhttp/JdkHttpServerFactory.java
+++ containers/jdk-http/src/main/java/org/glassfish/jersey/jdkhttp/JdkHttpServerFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2022 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -223,11 +223,15 @@ public final class JdkHttpServerFactory {
                 ? (isHttp ? Container.DEFAULT_HTTP_PORT : Container.DEFAULT_HTTPS_PORT)
                 : uri.getPort();
 
+        final InetSocketAddress socketAddress = (uri.getHost() == null)
+                ? new InetSocketAddress(port)
+                : new InetSocketAddress(uri.getHost(), port);
+
         final HttpServer server;
         try {
             server = isHttp
-                    ? HttpServer.create(new InetSocketAddress(port), 0)
-                    : HttpsServer.create(new InetSocketAddress(port), 0);
+                    ? HttpServer.create(socketAddress, 0)
+                    : HttpsServer.create(socketAddress, 0);
         } catch (final IOException ioe) {
             throw new ProcessingException(LocalizationMessages.ERROR_CONTAINER_EXCEPTION_IO(), ioe);
         }
