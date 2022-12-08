3a364bcd9bab1850faefbff683e0dd7876e81aaa
jansupol <jan.supol@oracle.com>
2022-12-08 13:47:31 +0100

    Store InvocationBuilderListenerStage into Runtime not to look up builder listeners each request
    Signed-off-by: jansupol <jan.supol@oracle.com>

--- core-client/src/main/java/org/glassfish/jersey/client/JerseyWebTarget.java
+++ core-client/src/main/java/org/glassfish/jersey/client/JerseyWebTarget.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2022 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -368,7 +368,7 @@ public class JerseyWebTarget implements javax.ws.rs.client.WebTarget, Initializa
     }
 
     private static JerseyInvocation.Builder onBuilder(JerseyInvocation.Builder builder) {
-        new InvocationBuilderListenerStage(builder.request().getInjectionManager()).invokeListener(builder);
+        builder.request().getClientRuntime().getInvocationBuilderListenerStage().invokeListener(builder);
         return builder;
     }
 }
