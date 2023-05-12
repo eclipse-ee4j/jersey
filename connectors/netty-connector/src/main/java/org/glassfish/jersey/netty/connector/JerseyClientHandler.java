4bdb30d76c7f30e71e957aa04b3fe6957c4d8394
jansupol <jan.supol@oracle.com>
2023-05-12 10:32:07 +0200

    Fix possible NPE in netty client
    Signed-off-by: jansupol <jan.supol@oracle.com>

--- connectors/netty-connector/src/main/java/org/glassfish/jersey/netty/connector/JerseyClientHandler.java
+++ connectors/netty-connector/src/main/java/org/glassfish/jersey/netty/connector/JerseyClientHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2022 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -188,6 +188,9 @@ class JerseyClientHandler extends SimpleChannelInboundHandler<HttpObject> {
 
             if (content.isReadable()) {
                 content.retain();
+                if (nis == null) {
+                    nis = new NettyInputStream();
+                }
                 nis.publish(content);
             }
 
