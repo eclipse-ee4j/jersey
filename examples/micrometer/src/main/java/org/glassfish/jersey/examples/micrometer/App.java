734e59d236555862a642f42a5a2aab55122f79d5
Maxim Nesen <maxim.nesen@oracle.com>
2023-10-18 17:13:27 +0200

    UserGuide and example extended for Micrometer integration (#5427)
    Signed-off-by: Maxim Nesen <maxim.nesen@oracle.com>

--- examples/micrometer/src/main/java/org/glassfish/jersey/examples/micrometer/App.java
+++ examples/micrometer/src/main/java/org/glassfish/jersey/examples/micrometer/App.java
@@ -2,50 +2,34 @@
  * Copyright (c) 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0, which is available at
- * http://www.eclipse.org/legal/epl-2.0.
+ * terms of the Eclipse Distribution License v. 1.0, which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
  *
- * This Source Code may also be made available under the following Secondary
- * Licenses when the conditions for such availability set forth in the
- * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
- * version 2 with the GNU Classpath Exception, which is available at
- * https://www.gnu.org/software/classpath/license.html.
- *
- * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ * SPDX-License-Identifier: BSD-3-Clause
  */
 
 package org.glassfish.jersey.examples.micrometer;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 
 import java.io.IOException;
 import java.net.URI;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.micrometer.server.DefaultJerseyTagsProvider;
-import org.glassfish.jersey.micrometer.server.MetricsApplicationEventListener;
-import org.glassfish.jersey.server.ResourceConfig;
-
-import org.glassfish.grizzly.http.server.HttpServer;
-
 public class App {
 
     private static final URI BASE_URI = URI.create("http://localhost:8080/micro/");
-    public static final String ROOT_PATH = "meter";
+    public static final String WEB_PATH = "/micro/";
 
     public static void main(String[] args) {
         try {
             System.out.println("Micrometer/ Jersey Basic Example App");
 
-            final MeterRegistry registry = new SimpleMeterRegistry();
-
-            final ResourceConfig resourceConfig = new ResourceConfig(MicrometerResource.class)
-                    .register(new MetricsApplicationEventListener(registry, new DefaultJerseyTagsProvider(),
-                    "http.shared.metrics", true))
-                    .register(new MetricsResource(registry));
-            final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, resourceConfig, false);
+            final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI,
+                    new MetricsResourceConfig(),
+                    false);
             Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -55,9 +39,9 @@ public class App {
             server.start();
 
             System.out.println(String.format("Application started.\nTry out                        %s%s\n"
-                            + "After several requests go to   %s%s\nAnd after that go to the       %s%s\n"
+                            + "And after that go to the       %s%s\n"
                             + "Stop the application using CTRL+C",
-                    BASE_URI, ROOT_PATH, BASE_URI, "metrics", BASE_URI, "metrics/metrics"));
+                    BASE_URI, "timed", BASE_URI, "metrics"));
             Thread.currentThread().join();
         } catch (IOException | InterruptedException ex) {
             Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
--- examples/micrometer/src/main/java/org/glassfish/jersey/examples/micrometer/MetricsResource.java
+++ examples/micrometer/src/main/java/org/glassfish/jersey/examples/micrometer/MetricsResource.java
@@ -2,71 +2,28 @@
  * Copyright (c) 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0, which is available at
- * http://www.eclipse.org/legal/epl-2.0.
+ * terms of the Eclipse Distribution License v. 1.0, which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
  *
- * This Source Code may also be made available under the following Secondary
- * Licenses when the conditions for such availability set forth in the
- * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
- * version 2 with the GNU Classpath Exception, which is available at
- * https://www.gnu.org/software/classpath/license.html.
- *
- * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ * SPDX-License-Identifier: BSD-3-Clause
  */
 
 package org.glassfish.jersey.examples.micrometer;
 
-import io.micrometer.core.instrument.Meter;
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Timer;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import java.util.concurrent.TimeUnit;
+
+import static org.glassfish.jersey.examples.micrometer.App.WEB_PATH;
 
 @Path("metrics")
 public class MetricsResource {
 
-    private final MeterRegistry registry;
-
-    public MetricsResource(MeterRegistry registry) {
-        this.registry = registry;
-    }
-
     @GET
-    @Produces("text/plain")
+    @Produces("text/html")
     public String getMeters() {
-        final StringBuffer result = new StringBuffer();
-        try {
-            result.append("Listing available meters: ");
-            for (final Meter meter : registry.getMeters()) {
-                result.append(meter.getId().getName());
-                result.append("; ");
-            }
-        } catch (Exception ex) {
-            System.out.println(ex);
-            result.append("Exception occured, see log for details...");
-            result.append(ex.toString());
-        }
-        return result.toString();
-    }
-    @GET
-    @Path("metrics")
-    @Produces("text/plain")
-    public String getMetrics() {
-        final StringBuffer result = new StringBuffer();
-        try {
-            final Timer timer = registry.get("http.shared.metrics")
-                    .tags("method", "GET", "uri", "/micro/meter", "status", "200", "exception", "None", "outcome", "SUCCESS")
-                    .timer();
-            result.append(String.format("Overall requests counts: %d, total time (millis): %f",
-                    timer.count(), timer.totalTime(TimeUnit.MILLISECONDS)));
-        } catch (Exception ex) {
-            System.out.println(ex);
-            result.append("Exception occured, see log for details...");
-            result.append(ex.toString());
-        }
-        return result.toString();
+       return "<html><body>Gaining measurements for the summary page, try <a href=\""
+               + WEB_PATH + "summary\">summary</a>. If you want more measurements just refresh this page several times."
+               + "</body></html>";
     }
 }
