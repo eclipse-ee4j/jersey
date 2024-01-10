3b1b53e4463b42921db377c42edf43dd10e33ff5
Arjan Tijms <arjan.tijms@omnifish.ee>
2024-01-10 13:40:22 +0100

    Update versions of dependencies in pom.xml (#5508)
    Signed-off-by: Arjan Tijms <arjan.tijms@omnifish.ee>
    Signed-off-by: Maxim Nesen <maxim.nesen@oracle.com>
    Co-authored-by: Maxim Nesen <maxim.nesen@oracle.com>

--- tests/osgi/functional/src/test/java/org/glassfish/jersey/osgi/test/util/Helper.java
+++ tests/osgi/functional/src/test/java/org/glassfish/jersey/osgi/test/util/Helper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2022 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2024 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -144,6 +144,7 @@ public class Helper {
                 // jakarta.annotation has to go first!
                 mavenBundle().groupId("jakarta.annotation").artifactId("jakarta.annotation-api").versionAsInProject(),
                 mavenBundle().groupId("jakarta.activation").artifactId("jakarta.activation-api").versionAsInProject(),
+                mavenBundle().groupId("jakarta.el").artifactId("jakarta.el-api").versionAsInProject(),
                 mavenBundle().groupId("jakarta.inject").artifactId("jakarta.inject-api").versionAsInProject(),
                 mavenBundle().groupId("jakarta.xml.bind").artifactId("jakarta.xml.bind-api").versionAsInProject(),
                 junitBundles(),
