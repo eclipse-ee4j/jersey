6ad2d5bf3544016f9fe396c9dfcf954aec53d512
jansupol <jan.supol@oracle.com>
2024-03-20 13:21:30 +0100

    Replace ThreadLocal with ConcurrentQueue in HttpDateFormat Introduce a common facade for SimpleDateFormat and DateTimeFormatter Able to switch to DateTimeFormatter for a small performance boost
    Signed-off-by: jansupol <jan.supol@oracle.com>

--- core-common/src/main/java/org/glassfish/jersey/message/internal/NewCookieProvider.java
+++ core-common/src/main/java/org/glassfish/jersey/message/internal/NewCookieProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2024 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -81,7 +81,7 @@ public class NewCookieProvider implements HeaderDelegateProvider<NewCookie> {
         }
         if (cookie.getExpiry() != null) {
             b.append(";Expires=");
-            b.append(HttpDateFormat.getPreferredDateFormat().format(cookie.getExpiry()));
+            b.append(HttpDateFormat.getPreferredDateFormatter().format(cookie.getExpiry()));
         }
 
         return b.toString();
