da8bbbee73a96c1570be0da01d1c398b25487301
rutterpaul-personal <133198135+rutterpaul-personal@users.noreply.github.com>
2024-04-24 11:45:50 +0200

    Update ChunkedInputOutputTest.java
    Update test

--- tests/e2e-server/src/test/java/org/glassfish/jersey/tests/e2e/server/ChunkedInputOutputTest.java
+++ tests/e2e-server/src/test/java/org/glassfish/jersey/tests/e2e/server/ChunkedInputOutputTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2024 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2022 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -68,8 +68,7 @@ public class ChunkedInputOutputTest extends JerseyTest {
         @GET
         @Path("/testWithBuilder")
         public ChunkedOutput<String> getWithBuilder() {
-            return getOutput(ChunkedOutput.<String>newBuilder(String.class).queueCapacity(2)
-                             .chunkDelimiter("\r\n".getBytes()).build());
+            return getOutput(ChunkedOutput.<String>builder(String.class).queueCapacity(2).chunkDelimiter("\r\n".getBytes()).build());
         }
 
         /**
