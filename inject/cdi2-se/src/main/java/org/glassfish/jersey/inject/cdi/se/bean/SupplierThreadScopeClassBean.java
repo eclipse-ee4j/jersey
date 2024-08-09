b752b3c0bc60a24a9a67a9bcb8d09b66b2e30d1e
jansupol <jan.supol@oracle.com>
2024-08-09 12:48:56 +0200

    Allow SupplierClassBinding in ThreadScope in CDI
    Signed-off-by: jansupol <jan.supol@oracle.com>

--- inject/cdi2-se/src/main/java/org/glassfish/jersey/inject/cdi/se/bean/SupplierThreadScopeClassBean.java
+++ inject/cdi2-se/src/main/java/org/glassfish/jersey/inject/cdi/se/bean/SupplierThreadScopeClassBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2024 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -14,32 +14,29 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
-package org.glassfish.jersey.inject.weld.internal.bean;
+package org.glassfish.jersey.inject.cdi.se.bean;
+
+import java.lang.annotation.Annotation;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
 
 import javax.enterprise.context.Dependent;
 import javax.enterprise.context.spi.CreationalContext;
-import javax.enterprise.inject.spi.Bean;
-import javax.enterprise.inject.spi.PassivationCapable;
-import javax.ws.rs.RuntimeType;
 
-import org.glassfish.jersey.inject.weld.internal.inject.InitializableSupplierInstanceBinding;
-import org.glassfish.jersey.inject.weld.internal.injector.JerseyInjectionTarget;
+import org.glassfish.jersey.internal.inject.SupplierClassBinding;
 import org.glassfish.jersey.internal.inject.SupplierInstanceBinding;
-import org.jboss.weld.bean.StringBeanIdentifier;
+import org.glassfish.jersey.internal.util.collection.LazyValue;
+import org.glassfish.jersey.internal.util.collection.Value;
+import org.glassfish.jersey.internal.util.collection.Values;
 import org.jboss.weld.bean.proxy.BeanInstance;
-import org.jboss.weld.bean.proxy.ContextBeanInstance;
 import org.jboss.weld.bean.proxy.ProxyFactory;
 import org.jboss.weld.manager.BeanManagerImpl;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
-import java.util.WeakHashMap;
-import java.util.function.Supplier;
 
 /**
- * Creates an implementation of {@link Bean} interface using Jersey's {@link SupplierInstanceBinding}.
- * Binding provides the information about the bean also called {@link javax.enterprise.inject.spi.BeanAttributes} information.
- * The {@code Bean} does not use {@link JerseyInjectionTarget} because serves already
+ * Creates an implementation of {@link jakarta.enterprise.inject.spi.Bean} interface using Jersey's {@link SupplierInstanceBinding}.
+ * Binding provides the information about the bean also called {@link jakarta.enterprise.inject.spi.BeanAttributes} information.
+ * The {@code Bean} does not use {@link org.glassfish.jersey.inject.cdi.se.injector.JerseyInjectionTarget} because serves already
  * created proxy, therefore the create operation just return provided instance without any other contextual operation
  * (produce, inject, destroy).
  * <p>
@@ -51,7 +48,7 @@ import java.util.function.Supplier;
  * AbstractBinder {
  *     &#64;Override
  *     protected void configure() {
- *         bindFactory(new MyFactoryInjectionProvider())
+ *         bindFactory(MyFactoryInjectionSupplier.class)
  *              .to(MyBean.class)
  *              .in(PerThread.class);
  *     }
@@ -66,25 +63,22 @@ import java.util.function.Supplier;
  * }
  * </pre>
  */
-public class InitializableSupplierThreadScopeBean extends JerseyBean<Object> {
+class SupplierThreadScopeClassBean extends JerseyBean<Object> {
+    private final LazyValue<ThreadScopeBeanInstance<Object>> beanInstance;
+    private final SupplierClassBinding binding;
+    private final LazyValue<Object> proxy;
+    private final AtomicReference<CreationalContext> creationalContextAtomicReference = new AtomicReference<>();
 
-    private final ThreadScopeBeanInstance<Object> beanInstance;
-    private final InitializableSupplierInstanceBinding binding;
-    private final Object proxy;
-
-    /**
-     * Creates a new Jersey-specific {@link Bean} instance.
-     *
-     * @param binding {@link javax.enterprise.inject.spi.BeanAttributes} part of the bean.
-     */
-    @SuppressWarnings("unchecked")
-    InitializableSupplierThreadScopeBean(RuntimeType runtimeType,
-                                         InitializableSupplierInstanceBinding binding,
-                                         BeanManagerImpl manager) {
-        super(runtimeType, binding);
+    SupplierThreadScopeClassBean(SupplierClassBinding binding, SupplierClassBean supplierClassBean, BeanManagerImpl beanManager) {
+        super(binding);
         this.binding = binding;
-        this.beanInstance = new ThreadScopeBeanInstance<>(binding.getSupplier(), this, manager.getContextId());
-        this.proxy = createClientProxy(beanInstance, manager.getContextId());
+        this.beanInstance = Values.lazy((Value<ThreadScopeBeanInstance<Object>>) () -> {
+            Supplier supplierInstance = supplierClassBean.create(creationalContextAtomicReference.get());
+            ThreadScopeBeanInstance scopeBeanInstance =
+                    new ThreadScopeBeanInstance(supplierInstance, this, beanManager.getContextId());
+            return scopeBeanInstance;
+        });
+        this.proxy = Values.lazy((Value<Object>) () -> createClientProxy(beanInstance.get(), beanManager.getContextId()));
     }
 
     @Override
@@ -94,12 +88,15 @@ public class InitializableSupplierThreadScopeBean extends JerseyBean<Object> {
 
     @Override
     public Object create(CreationalContext<Object> ctx) {
-        return proxy;
+        creationalContextAtomicReference.set(ctx);
+        return proxy.get();
     }
 
     @Override
     public void destroy(Object instance, CreationalContext<Object> creationalContext) {
-        this.beanInstance.dispose();
+        if (beanInstance.isInitialized()) {
+            this.beanInstance.get().dispose();
+        }
     }
 
     @Override
