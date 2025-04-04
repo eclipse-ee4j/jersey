a75d1799cf7a0431c41039ba87197103289fb5b1
allebaton <20238592+allebaton@users.noreply.github.com>
2025-04-04 12:39:17 +0300

    Fixed reconnectDelay
    Author: allebaton <allebaton@gmail.com>
    
    reconnectUnit was not  properly. Fix sets reconnectUnit to the value of input parameter "unit"

--- media/sse/src/main/java/org/glassfish/jersey/media/sse/internal/EventProcessor.java
+++ media/sse/src/main/java/org/glassfish/jersey/media/sse/internal/EventProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2024 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2025 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -400,7 +400,7 @@ public class EventProcessor implements Runnable, EventListener {
          */
         public Builder reconnectDelay(long reconnectDelay, TimeUnit unit) {
             this.reconnectDelay = reconnectDelay;
-            this.reconnectUnit = reconnectUnit;
+            this.reconnectUnit = unit;
             return this;
         }
 
