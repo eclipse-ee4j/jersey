65b1bd1e04ff4b0714a6e8b63911fa7c2bc291b9
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2025-07-21 13:25:31 +0200

    Useless exception when client closes stream whilst response transfer #3459
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- core-server/src/main/java/org/glassfish/jersey/server/ServerRuntime.java
+++ core-server/src/main/java/org/glassfish/jersey/server/ServerRuntime.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2024 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2025 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -720,7 +720,7 @@ public class ServerRuntime {
                             // output streams writes out bytes only on close (for example GZipOutputStream).
                             response.close();
                         } catch (final Exception e) {
-                            LOGGER.log(Level.SEVERE, LocalizationMessages.ERROR_CLOSING_COMMIT_OUTPUT_STREAM(), e);
+                            LOGGER.log(Level.WARNING, LocalizationMessages.ERROR_CLOSING_COMMIT_OUTPUT_STREAM(), e);
                         }
                     }
                 }
