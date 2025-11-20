8e9e68035803ddf46e9dcb1f794d403e990bd4f7
Maxim Nesen <senivam@gmail.com>
2025-11-20 12:12:47 +0100

    Exclude jersey-constants from the release test
    Signed-off-by: Maxim Nesen <senivam@gmail.com>

--- tests/release-test/src/test/java/org/glassfish/jersey/test/artifacts/ManifestTest.java
+++ tests/release-test/src/test/java/org/glassfish/jersey/test/artifacts/ManifestTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, 2024 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, 2025 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -37,6 +37,7 @@ public class ManifestTest {
             "servlet-portability", /* obsolete */
             "helidon-connector", /* Helidon does not contain OSGi headers */
             "grizzly-connector", /* Limited maintenance */
+            "jersey-constants"   /* Constants do not contain OSGi headers */
     };
 
     @Test
